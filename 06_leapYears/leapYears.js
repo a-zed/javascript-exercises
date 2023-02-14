d53cb93baaf3698b4e644fcd1a9b287cd9c84a88
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-14 17:09:05 +0700

    feat: Add leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // if year is divisible by 4 and year is not divisible by 100 or divisible by 400 return true
+    if (year % 4 === 0 &&
+       (year % 100 !== 0 || year % 400 === 0)){
+
+            return true;
+    }
+
+    return false;
 
 };
 
