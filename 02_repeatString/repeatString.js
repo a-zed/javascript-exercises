adaa6573df578572ef708750a27de3983a51fe23
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-14 11:02:13 +0700

    feat: Add helloWorld and repeatString functions
    This commit submits both functions, which have both passed all TOP generated tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(phrase, timesRepeated) {
+
+    if (timesRepeated < 0){
+        return "ERROR";
+    }
+
+    let generatedString = "";
+
+    for (let i = 1; i <= timesRepeated; i++){
+
+        generatedString = phrase + generatedString
+    }
+
+    return generatedString;
 
 };
 
