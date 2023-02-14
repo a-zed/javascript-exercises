53cbed9bfa48912ac5b9e0c341ba538ae6f458a6
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-14 20:29:09 +0700

    feat: Add calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbersToSum) {
+  let sum = 0;
+	for (let number of numbersToSum){
+    sum = sum + parseInt(number);
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbersToMultiply) {
+  let total = 1;
+	for (let number of numbersToMultiply){
+    total = total * parseInt(number);
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i = num; i > 0; i--){
+    total = total * i;
+  }
+	return total;
 };
 
 // Do not edit below this line
