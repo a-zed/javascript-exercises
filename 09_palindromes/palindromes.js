acb8ea8d4b5e32aff3425acf4fd845d0c84c5c24
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-14 22:35:07 +0700

    feat: Add palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let stringRefined = string.toLowerCase().replace(/[^a-z]/g, "");
+    return stringRefined == stringRefined.split("").reverse().join("");
 };
 
 // Do not edit below this line
