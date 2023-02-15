cc5e3b7df726f7aef338786b276ae2deda5d51bb
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-15 14:57:01 +0700

    feat: Add findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    // create variable to store age of current person and oldest person id
+    let currentPersonAge;
+    let oldestPersonAge = 0;
+    let oldestPersonId;
+
+    // look through each person in array
+    for (let i = 0; i < people.length; i++){
+
+        // if person is not dead
+        if (people[i].yearOfDeath == undefined){
+
+            currentPersonAge = new Date().getFullYear() - people[i].yearOfBirth;
+        }
+
+        // else they are dead
+        else {
+            currentPersonAge = people[i].yearOfDeath - people[i].yearOfBirth
+        }
+
+        // if older than oldest person logged so far, update oldest age person and id variables
+        if (currentPersonAge > oldestPersonAge){
+
+            oldestPersonAge = currentPersonAge;
+            oldestPersonId = i;
+        }
+    }
+
+    return people[oldestPersonId];
 
 };
 
