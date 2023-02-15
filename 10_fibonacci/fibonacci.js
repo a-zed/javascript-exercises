c27ff20b4b8d2cf8659d9d5f531b05f0c4d2b18f
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-15 11:34:25 +0700

    feat: Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    // if number is negative return oops
+    if (num < 0){
+        return "OOPS";
+    }
+
+    // Declare variables to store previous, current and next fibonacci numbers
+    let previousFibonacci = 0;
+    let currentFibonacci = 1;
+    let nextFibonacci;
+
+    // Loop through sequence until num reached
+    for (let i = 1; i < num; i++){
+
+        // Update fibonacci values as it moves through the sequence
+        nextFibonacci = previousFibonacci + currentFibonacci;
+        previousFibonacci = currentFibonacci;
+        currentFibonacci = nextFibonacci;
+    }
+
+    return currentFibonacci;
 
 };
 
